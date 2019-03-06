ea9987d3925f2f4a23c7e8458b42c9273254132a
Nicholas Devenish <ndevenish@gmail.com>
2019-03-06 16:58:23 +0000

    merge show stuff to fixer

--- fix_show.py
+++ fix_show.py
@@ -13,6 +13,7 @@ Options:
 """
 
 import sys
+import itertools
 
 from bowler import Query
 from bowler.types import LN, Capture, Filename
@@ -22,7 +23,7 @@ from fissix.pytree import type_repr, Node, Leaf
 from fissix.pgen2 import token
 from fissix.fixer_util import Name, LParen, RParen, find_indentation, touch_import
 
-from typing import Optional, List
+from typing import Optional, List, Tuple
 
 
 import fissix.pygram
@@ -91,9 +92,252 @@ def get_child(node: Node, childtype: int) -> Optional[LN]:
     return filt[0] if filt else None
 
 
-def get_children(node: Node, childtype: int) -> List[LN]:
+def get_children(
+    node: Node, childtype: int, *, recursive: bool = False, recurse_if_found=False
+) -> List[LN]:
     """Extract all children from a node that match a type"""
-    return [x for x in node.children if x.type == childtype]
+    if not recursive:
+        return [x for x in node.children if x.type == childtype]
+    matches = []
+    for child in node.children:
+        if child.type == childtype:
+            matches.append(child)
+            # If we want to stop recursing into found nodes
+            if recurse_if_found:
+                continue
+        matches.extend(get_children(child, childtype, recursive=True))
+    return matches
+
+
+def get_print_file(node: Node) -> Optional[LN]:
+    """Given a print node, get the output file.
+
+    Returns:
+        A node representing the name/destination, or None if not specified
+    """
+    if node.type == python_symbols.print_stmt:
+        if node.children[1].type == token.RIGHTSHIFT:
+            return node.children[2]
+        return None
+    elif node.type == python_symbols.power:
+        # If not an argument list, then we know that we are sending to stdout
+        argument = node.children[1].children[1]
+        if argument.type == python_symbols.argument:
+            arg_name, _, arg_val = _extract_argument(argument)
+            if arg_name == "file":
+                return arg_val
+        elif argument.type == python_symbols.arglist:
+            # Argument lists should be almost identical?
+            args = _split_arglist(argument)
+            for arg_name, _, arg_val in args:
+                if arg_name == "file":
+                    return arg_val
+        else:
+            return None
+    else:
+        raise RuntimeError("Unknown print statement type?")
+
+
+class UnableToTransformError(Exception):
+    pass
+
+
+def analyse_show(node: Node, filename: Filename) -> Optional[str]:
+    """Analyse a show function to determine how to call it.
+
+    Arguments:
+        node: A funcdef Node
+
+    Returns:
+        The name of the keyword argument to pass file reader, or None if
+        the function writes to stdout.
+
+    Raises:
+        UnableToTransformError: Function is too complicated to analyse
+    """
+    showID = "{}:{}".format(filename, node.get_lineno())
+    # if "cctbx/crystal/__init__.py" in filename:
+    #     breakpoint()
+    # Find calls to print in here
+    # Firstly, print statements
+    print_calls = get_children(node, python_symbols.print_stmt, recursive=True)
+    # Now anything that looks like a print function
+    print_calls.extend(
+        [
+            x
+            for x in get_children(node, python_symbols.power, recursive=True)
+            if str(x.children[0]).strip() == "print"
+        ]
+    )
+
+    if not print_calls:
+        raise UnableToTransformError("Could not find any output calls")
+
+    dest_file_nodes = [get_print_file(x) for x in print_calls]
+    dest_file_vals = {
+        str(x).strip() if x is not None else None for x in dest_file_nodes
+    }
+    print(
+        "Output destinations:",
+        ", ".join(str(x) if x is not None else "sys.stdout" for x in dest_file_vals),
+    )
+    # Require a single output destination for now.
+    if not len(dest_file_vals) <= 1:
+        raise UnableToTransformError(
+            "Multiple output destinations: " + ", ".join(str(x) for x in dest_file_vals)
+        )
+
+    # The actual output stream used to output
+    output_name = next(iter(dest_file_vals))
+
+    # If this single output is 'None' then we print to stdout
+    if output_name is None:
+        print("PASS: {}: Writes to stdout only".format(showID))
+    else:
+        params = _get_function_arguments(node)
+        without_default = {
+            name for name, _, _ in itertools.takewhile(lambda x: not x[1], params[1:])
+        }
+        with_default = {
+            name for name, _, _ in itertools.takewhile(lambda x: x[1], params[1:])
+        }
+
+        if without_default:
+            # We have required parameters. Check that it's just the log stream parameter
+            if not without_default == {output_name}:
+                raise UnableToTransformError(
+                    "Unexpected extra required parameter: {} vs writes to {}".format(
+                        without_default, output_name
+                    )
+                )
+            print(
+                "PASS: {}: Writes to required argument {}".format(
+                    showID, without_default & {output_name}
+                )
+            )
+        elif with_default:
+            # We don't have any required parameters. Check that we pass the stream in as a keyword
+            if not {output_name} <= with_default:
+                raise UnableToTransformError(
+                    "No function arguments match stream write destination"
+                )
+            print(
+                "PASS: {}: Writes to keyword argument {} (of {})".format(
+                    showID, output_name, with_default
+                )
+            )
+        else:
+            # breakpoint()
+            assert len(params) <= 1
+            raise UnableToTransformError("Doesn't write to stdout; but no arguments?")
+
+    return output_name
+
+
+def split_list_on(collection, condition):
+    entries = []
+    current_entry = []
+    for i, item in enumerate(collection):
+        if condition(item):
+            entries.append(current_entry)
+            current_entry = []
+            # If at the end, then we need to append an empty list
+            if i == len(collection) - 1:
+                entries.append([])
+        else:
+            current_entry.append(item)
+    if current_entry:
+        entries.append(current_entry)
+    return entries
+
+
+def test_split_list_on():
+    assert split_list_on("123,43,5,", lambda x: x == ",") == [
+        ["1", "2", "3"],
+        ["4", "3"],
+        ["5"],
+        [],
+    ]
+
+
+def _split_arglist(node: Node):
+    """Splits a typedarglist into separate data"""
+    if node.type == python_symbols.argument:
+        return [_handle_argument(node)]
+
+    assert node.type == python_symbols.arglist
+    entries = split_list_on(node.children, lambda x: x.type == token.COMMA)
+
+    converted = []
+    for entry in entries:
+        assert len(entry) == 1
+        entry = entry[0]
+        if entry.type == python_symbols.argument:
+            converted.append(_extract_argument(entry))
+        else:
+            converted.append((str(entry).strip(), False, None))
+
+    return converted
+    # return entries
+
+
+def _extract_argument(node):
+    assert node.type == python_symbols.argument
+    if node.children[1].type == token.EQUAL:
+        return (node.children[0].value, True, node.children[2])
+    else:
+        return (str(node).strip(), False, None)
+
+
+def _split_typedargslist(node: Node):
+    """Splits a typedarglist into separate data"""
+    assert node.type == python_symbols.typedargslist
+    # entries = []
+    # index = 0
+    # current_entry = []
+    # while index < len(node.children):
+    #     part = node.children[index]
+    #     if part.type == token.COMMA:
+    #         entries.append(current_entry)
+    #         current_entry = []
+    #     else:
+    #         current_entry.append(part)
+    #     index += 1
+    # if current_entry:
+    #     entries.append(current_entry)
+    entries = split_list_on(node.children, lambda x: x.type == token.COMMA)
+
+    converted = []
+    for entry in entries:
+        if len(entry) == 1:
+            converted.append((entry[0].value, False, None))
+        elif len(entry) == 3:
+            assert entry[1].type == token.EQUAL
+            converted.append((entry[0].value, True, entry[2]))
+        else:
+            raise RuntimeError(
+                "Don't understand typedarglist parameter {}".format(entry)
+            )
+    return converted
+    # return entries
+
+
+def _get_function_arguments(node):
+    """
+    Returns:
+        (name, has_default, default)
+    """
+    if node.type == python_symbols.funcdef:
+        node = node.children[2]
+    else:
+        assert node.type == python_symbols.parameters
+    #
+    if node.children[1].type == python_symbols.typedargslist:
+        return _split_typedargslist(node.children[1])
+    elif node.children[1].type == token.NAME:
+        return [(node.children[1].value, False, None)]
+
+    raise RuntimeError("Unknown configuration for python function parameters")
 
 
 def get_trailing_text_node(node: Node) -> Leaf:
@@ -141,21 +385,45 @@ def split_suffix(leaf: Leaf) -> Tuple[str, str]:
 
 def process_class(node: LN, capture: Capture, filename: Filename) -> Optional[LN]:
     """Do the processing/modification of the class node"""
-    print("show(): {}:{}".format(filename, node.get_lineno()))
-    suite = get_child(node, python_symbols.suite)
+    print("Class for show(): {}:{}".format(filename, node.get_lineno()))
 
+    suite = get_child(node, python_symbols.suite)
     # Get the suite indent
     indent = find_indentation(suite)
 
+    # Find the show() function
+    # breakpoint()
+    funcs = {
+        x.children[1].value: x for x in get_children(suite, python_symbols.funcdef)
+    }
+    show_func = funcs["show"]
+    show_keyword = analyse_show(show_func, filename)
+
+    if show_keyword is None:
+        # Use the stdout replacement method
+        kludge_text = "def __str__(self):\n{0}  return kludge_show_to_str(self)\n\n".format(
+            indent
+        )
+    else:
+        kludge_text = "def __str__(self):\n{0}  out = StringIO()\n{0}  self.show({1}=out)\n{0}  return out.getvalue.rstrip()\n\n".format(
+            indent, show_keyword
+        )
+    # from six.moves import cStringIO as StringIO
+
+    #   out = StringIO()
+
+    #   stdout = sys.stdout
+    #   sys.stdout = out
+
+    #   try:
+    #     obj.show()
+    #   finally:
+    #     sys.stdout = stdout
+
+    #   return out.getvalue().rstrip()
+
     # To avoid having to work out indent correction, just generate with correct
-    kludge_node = get_child(
-        driver.parse_string(
-            "def __str__(self):\n{0}  return kludge_show_to_str(self)\n\n".format(
-                indent
-            )
-        ),
-        python_symbols.funcdef,
-    )
+    kludge_node = get_child(driver.parse_string(kludge_text), python_symbols.funcdef)
 
     # Work out if we have any trailing text/comments that need to be moved
     trail_node = get_trailing_text_node(suite)
@@ -181,8 +449,11 @@ def process_class(node: LN, capture: Capture, filename: Filename) -> Optional[LN
     # Get the kludge dedent - now the last dedent
     kludge_dedent = kludge_node.children[-1].children[-1]
     kludge_dedent.prefix = post
-    # Make sure the function is available
-    touch_import("libtbx.utils", "kludge_show_to_str", node)
+    # Make sure the functions used are available
+    if show_keyword is None:
+        touch_import("libtbx.utils", "kludge_show_to_str", node)
+    else:
+        touch_import("six.moves", "StringIO", node)
 
 
 def do_filter(node: LN, capture: Capture, filename: Filename) -> bool:
